76fb0fc2d6f27e60169cdba7567f5d8b5b2ad3aa
Ryley Watson <ryleyswatson@gmail.com>
2024-12-19 08:53:55 -0800

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats=["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(moreCats){
+    return [...cats,"Broom"];
+}
+
+function prependCat(evenMoreCats){
+    return ["Arnold",...cats]
+}
+
+function removeLastCat(oneLessCat){
+    return cats.slice(0,cats.length-1)
+}
+
+function removeFirstCat(oneLessCatv2){
+    return cats.slice(1)
+}
+
